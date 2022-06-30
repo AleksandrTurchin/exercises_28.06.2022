5c383f312c886a94f8e395a54d80ca12b871f614
AleksandrTurchin <107668514+AleksandrTurchin@users.noreply.github.com>
2022-06-30 10:47:42 +0300

    Create task1.py
     Пользователь вводит строку и символ. Определить количество вхождений символа в строку, независимо от регистра, при этом нельзя использовать метод count.

---
+++ task1.py
@@ -0,0 +1,9 @@
+string = input('Введите строку: ').lower()  # use lower() - convert to lowercase
+symbol = input('Введите символ: ').lower()
+
+counter = 0
+
+for i in string:
+    if i == symbol:
+        counter += 1  
+print(f'Количесвто вхождений в символа в строку: {counter}')
